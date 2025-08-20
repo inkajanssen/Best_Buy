c6357d68ba6b5bc933602f53a54e8337b2e9b181
inkajanssen <ijanssenuk@gmail.com>
2025-08-20 16:19:48 +0200

    deleted not needed code

--- products.py
+++ products.py
@@ -1,6 +1,3 @@
-from unicodedata import digit
-
-
 class Product:
 
     def __init__(self, name, price, quantity):
@@ -100,18 +97,3 @@ class Product:
         new_quantity = self.quantity - quantity
         Product.set_quantity(self, new_quantity)
         return price
-
-
-
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-bose.show()
-mac.show()
-
-bose.set_quantity(1000)
-bose.show()
